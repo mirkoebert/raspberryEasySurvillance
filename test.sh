1c9610d8bd0fef4980de4c11c0c7a6682c255480
Mirko Ebert <mirko.ebert@gmail.com>
2020-01-02 11:13:34 +0100

    introduce assert into test script

--- test.sh
+++ test.sh
@@ -1,14 +1,58 @@
 #!/bin/bash
 
-echo `date` Test 1: 
-rm -f cam/*.jpg
-rm -f blur/*.jpg
+function prepare {        
+	echo -----------------------------
+	echo `date` Test: $1 
+	rm -f cam/*.jpg
+	rm -f blur/*.jpg
+	rm -f ftp/*.jpg
+	rm -f stateMotionDected
+}
 
+function assertEquals {
+  echo `date` Result: "$1"
+  if [ "$1" = "$2" ]; then
+	echo Ok
+  else
+	echo Failed
+  fi
+}
+
+prepare 1	
 cp testPic/image.jpg cam
 ./normaize.sh
 cp testPic/image2.jpg cam
-
 ./normaize.sh
+
 result=$(./detectMotion.sh)
-echo `date` Result: $result
+assertEquals "No Motion" "$result"
+
+
+prepare 2	
+cp testPic/image2.jpg cam
+./normaize.sh
+cp testPic/image3.jpg cam
+./normaize.sh
+
+result=$(./detectMotion.sh)
+assertEquals "No Motion" "$result"
+
+
+prepare 3	
+cp testPic/image4.jpg cam
+./normaize.sh
+cp testPic/image3.jpg cam
+./normaize.sh
+
+result=$(./detectMotion.sh)
+assertEquals "No Motion" "$result"
+
+prepare 4	
+cp testPic/image4.jpg cam
+./normaize.sh
+cp testPic/image4X.jpg cam
+./normaize.sh
+
+result=$(./detectMotion.sh)
+assertEquals "Motion detected" "$result"
 
