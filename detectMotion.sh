8e00cf7a067ff6e40487e4cd0bee266aec57edb5
Mirko Ebert <mirko.ebert@gmail.com>
2020-01-03 21:58:49 +0100

    use faster metric

--- detectMotion.sh
+++ detectMotion.sh
@@ -6,19 +6,22 @@ file2Cache=`ls -r blur/*.cache | head -n 2 | tail -n 1`
 latestImage=`ls -r blur/*.mpc | head -n 1`
 
 
-val=$(compare  -fuzz 10% -metric RMSE $file2 $latestImage /dev/null 2>&1)
+#val=$(compare  -fuzz 10% -metric RMSE $file2 $latestImage null 2>&1)
+val=$(compare  -fuzz 10% -metric AE $file2 $latestImage null 2>&1)
 
 rm $file2
 rm $file2Cache
-val2=$(echo $val | awk '{print $1}')
+#val2=$(echo $val | awk '{print $1}')
 
 
-limit=791.4
-x=$(echo $val2'>'$limit | bc -l)
+#limit=1000
+#limit=791.4
+#x=$(echo $val2'>'$limit | bc -l)
 
 latestImage=`ls -r cam/ | head -n 1`
 FILE=stateMotionDected
-if [ "$x" -eq "0" ]; then
+if [ "$val" -lt "1000" ]; then
+#if [ "$x" -eq "0" ]; then
    echo "No Motion";
 else
    if test -f "$FILE"; then
