8f8c57ea6ccd31635de708e19e9a4d030d035c47
Mirko Ebert <mirko.ebert@gmail.com>
2020-01-03 22:08:44 +0100

    clean up

--- detectMotion.sh
+++ detectMotion.sh
@@ -6,22 +6,16 @@ file2Cache=`ls -r blur/*.cache | head -n 2 | tail -n 1`
 latestImage=`ls -r blur/*.mpc | head -n 1`
 
 
-#val=$(compare  -fuzz 10% -metric RMSE $file2 $latestImage null 2>&1)
 val=$(compare  -fuzz 10% -metric AE $file2 $latestImage null 2>&1)
 
 rm $file2
 rm $file2Cache
-#val2=$(echo $val | awk '{print $1}')
 
-
-#limit=1000
-#limit=791.4
-#x=$(echo $val2'>'$limit | bc -l)
 
 latestImage=`ls -r cam/ | head -n 1`
+file2Image=`ls -r cam/ | head -n 2 | tail -n 1`
 FILE=stateMotionDected
 if [ "$val" -lt "1000" ]; then
-#if [ "$x" -eq "0" ]; then
    echo "No Motion";
 else
    if test -f "$FILE"; then
@@ -31,10 +25,12 @@ else
     echo "Motion detected"
     touch $FILE 
     mv cam/$latestImage ftp/
+    mv cam/$file2Image ftp/
     exit;
    fi
 fi
 
-rm cam/$latestImage
+rm cam/$file2Image
+#rm cam/$latestImage
 
 
