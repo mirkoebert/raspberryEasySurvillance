d7f558a8942a34a928212eb9a2fe93717605755a
Mirko Ebert <mirko.ebert@gmail.com>
2020-01-04 21:55:54 +0100

    var optimization

--- run2.sh
+++ run2.sh
@@ -18,30 +18,27 @@ convert -colorspace LinearGray -normalize -blur 2x2  cam/$DATE blur/$DATE.mpc
 
 
 
-
 file2=`ls -r blur/*.mpc | head -n 2 | tail -n 1`
-file2Cache=`ls -r blur/*.cache | head -n 2 | tail -n 1`
 latestImage="blur/$DATE.mpc"
 
 
 val=$(compare  -fuzz 10% -metric AE $file2 $latestImage null 2>&1)
 
+file2Cache=`ls -r blur/*.cache | head -n 2 | tail -n 1`
 rm "$file2" "$file2Cache"
 
-latestImage="cam/$DATE"
-#latestImage=`ls -r cam/ | head -n 1`
 file2Image=`ls -r cam/ | head -n 2 | tail -n 1`
-FILE=stateMotionDected
 if [ "$val" -lt 1000 ]; then
    echo "No Motion";
 else
+   FILE=stateMotionDected
    if test -f "$FILE"; then
     echo "Motion ends"
     rm "$FILE"
    else
     echo "Motion detected"
     touch $FILE 
-    cp "cam/$latestImage" "ftp/"
+    cp "cam/$DATE" "ftp/"
     mv "cam/$file2Image" "ftp/"
     exit;
    fi
