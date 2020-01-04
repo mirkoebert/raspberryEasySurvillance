5a52e2154ed25c7a903d17d87d369ed320c6d77e
Mirko Ebert <mirko.ebert@gmail.com>
2020-01-04 12:02:15 +0100

    cleanup script

--- detectMotion.sh
+++ detectMotion.sh
@@ -8,9 +8,7 @@ latestImage=`ls -r blur/*.mpc | head -n 1`
 
 val=$(compare  -fuzz 10% -metric AE $file2 $latestImage null 2>&1)
 
-rm $file2
-rm $file2Cache
-
+rm "$file2" "$file2Cache"
 
 latestImage=`ls -r cam/ | head -n 1`
 file2Image=`ls -r cam/ | head -n 2 | tail -n 1`
@@ -20,17 +18,17 @@ if [ "$val" -lt "1000" ]; then
 else
    if test -f "$FILE"; then
     echo "Motion ends"
-    rm $FILE
+    rm "$FILE"
    else
     echo "Motion detected"
     touch $FILE 
-    cp cam/$latestImage ftp/
-    mv cam/$file2Image ftp/
+    cp "cam/$latestImage" "ftp/"
+    mv "cam/$file2Image" "ftp/"
     exit;
    fi
 fi
 
-rm cam/$file2Image
+rm "cam/$file2Image"
 #rm cam/$latestImage
 
 
