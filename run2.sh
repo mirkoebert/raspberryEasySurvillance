000194df0c6441b87aa977b685a906c4ee55572f
Mirko Ebert <mirko.ebert@gmail.com>
2020-01-04 22:19:07 +0100

    optimization

--- run2.sh
+++ run2.sh
@@ -6,6 +6,7 @@ if [ ! -z "$1" ]
 fi
 cd '/home/pi/raspberryEasySurvillance'
 
+file2Image=`ls -r cam/ | head -n 1`
 
 
 DATE=$(date +"%Y-%m-%d_%H%M%S").jpg
@@ -13,21 +14,18 @@ raspistill --width 1296 --height 972 --timeout 1  --nopreview -o cam/$DATE
 
 
 
-convert -colorspace LinearGray -normalize -blur 2x2  cam/$DATE blur/$DATE.mpc
-
+latestNImage="blur/$DATE.mpc"
+convert -colorspace LinearGray -normalize -blur 2x2  cam/$DATE $latestNImage
 
 
 
 file2=`ls -r blur/*.mpc | head -n 2 | tail -n 1`
-latestImage="blur/$DATE.mpc"
-
-
-val=$(compare  -fuzz 10% -metric AE $file2 $latestImage null 2>&1)
+val=$(compare  -fuzz 10% -metric AE $file2 $latestNImage null 2>&1)
 
 file2Cache=`ls -r blur/*.cache | head -n 2 | tail -n 1`
 rm "$file2" "$file2Cache"
 
-file2Image=`ls -r cam/ | head -n 2 | tail -n 1`
+
 if [ "$val" -gt 1000 ]; then
    FILE=stateMotionDected
    if test -f "$FILE"; then
