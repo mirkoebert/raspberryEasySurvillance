c19ccb5a848dce2ccc1ede4281a0744424132bca
Mirko Ebert <mirko.ebert@gmail.com>
2020-08-23 18:24:17 +0200

    add echo to enable and start service

--- install.sh
+++ install.sh
@@ -2,10 +2,6 @@
 #set -x
 
 #run scripts with sudo
-if [ "$EUID" -ne 0 ]
-  then echo "Please run script with sudo"
-  exit
-fi
 
 echo "Install software dependencies"
 apt-get --yes install imagemagick tree ftp
@@ -13,6 +9,13 @@ apt-get --yes install imagemagick tree ftp
 echo "Configure Service"
 cp survillancecam.service /etc/systemd/system/
 
-echo "Start Service"
+echo "Enable Service at System Start"
+echo 
+echo sudo systemctl enable survillancecam.service
+echo
+echo "Star" Service"
+echo
+echo "sudo systemctl start survillancecam.service"
+echo 
 
 
