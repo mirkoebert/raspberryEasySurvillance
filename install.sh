823f4f92c6061019a090adec4c01e21a0a5aa74c
Mirko Ebert <mirko.ebert@gmail.com>
2020-08-23 18:17:15 +0200

    add sudo check

--- install.sh
+++ install.sh
@@ -1,5 +1,18 @@
 #!/bin/dash
 #set -x
 
+#run scripts with sudo
+if [ "$EUID" -ne 0 ]
+  then echo "Please run script with sudo"
+  exit
+fi
+
+echo "Install software dependencies"
 apt-get --yes install imagemagick tree ftp
+
+echo "Configure Service"
+cp survillancecam.service /etc/systemd/system/
+
+echo "Start Service"
+
 
