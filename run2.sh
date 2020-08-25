0fc6f2f1de4e123e35f09c0ab30a29f009cbd770
Mirko Ebert <mirko.ebert@gmail.com>
2020-08-25 21:14:26 +0200

    reduce image size

--- run2.sh
+++ run2.sh
@@ -6,7 +6,7 @@ prevImage=`ls -r cam/ | head -n 1`
 camid=`hostname`
 
 DATE=$(date +"%Y-%m-%d_%H:%M:%S")_$camid.jpg
-raspistill --width 1296 --height 972 --timeout 1  --nopreview -o cam/$DATE
+raspistill --width 1296 --height 972 --timeout 1  --nopreview --quality 50  -o cam/$DATE
 
 
 
