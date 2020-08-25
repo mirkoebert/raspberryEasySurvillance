994b06dae97122521d8279258b1e4b03eb93ee90
Mirko Ebert <mirko.ebert@gmail.com>
2020-08-25 12:56:21 +0200

    use curl for ftp upload

--- run2.sh
+++ run2.sh
@@ -33,7 +33,8 @@ if [ ! -z "$prevImage" ]; then
 			touch $FILE 
 			cp "cam/$DATE" "ftp/"
 			mv "cam/$prevImage" "ftp/"
-			lftp -e "put -O / ftp/$prevImage ; bye "  $FTP_SERVER_RECORDINGS
+			curl -n -q -T ftp/$prevImage $FTP_SERVER_RECORDINGS
+			#lftp -e "put -O / ftp/$prevImage ; bye "  $FTP_SERVER_RECORDINGS
 			rm ftp/$prevImage
 			exit;
 		fi
