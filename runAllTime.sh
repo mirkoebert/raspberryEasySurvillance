1050dae0a96dc5d3033f68ff8097d94f88aee1ea
Mirko Ebert <mirko.ebert@gmail.com>
2020-12-09 19:23:44 +0100

    rescue old motion images in case of an restart

--- runAllTime.sh
+++ runAllTime.sh
@@ -2,12 +2,15 @@
 #set -x
 cd /home/pi/raspberryEasySurvillance || exit 1
 
+# rescue old motion images
+mkdir -p resucued
+mv ftp/*.jpg rescued
+
 # clean up
 rm -f stateMotionDected
 rm -f cam/*.*
 rm -f blur/*.*
 rm -r ftp/*.*
-rm -r www/*.*
 
 while true; do
   ./runOnce.sh
