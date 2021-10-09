cfc49a07a067ddd53e9ed77c674d6a5313a6d5f4
Mirko Ebert <mirko.ebert@gmail.com>
2021-10-09 17:05:52 +0200

    add router ip

--- src/reconnectWifi.sh
+++ src/reconnectWifi.sh
@@ -2,6 +2,7 @@
 
 # The IP for the server you wish to ping (8.8.8.8 is a public Google DNS server)
 SERVER=8.8.8.8
+#SERVER=192.168.0.1
 
 # Only send two pings, sending output to /dev/null
 ping -c2 ${SERVER} > /dev/null
@@ -9,7 +10,7 @@ ping -c2 ${SERVER} > /dev/null
 # If the return code from ping ($?) is not 0 (meaning there was an error)
 if [ $? != 0 ]
 then
-    # Restart the wireless interface
+    echo "Restart the wireless interface"
     ifdown --force wlan0
     sleep 5
     ifup --force wlan0
