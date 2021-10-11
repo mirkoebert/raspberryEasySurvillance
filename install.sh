ee2bf298c038bbcd7b3c03260ebcff531c012220
Mirko Ebert <mirko.ebert@gmail.com>
2021-10-11 20:56:46 +0200

    simplify script

--- install.sh
+++ install.sh
@@ -5,37 +5,38 @@
 if [[ `id -u` -ne 0 ]] ; then echo "Please run this script as root" ; exit 1 ; fi
 
 echo "Install software dependencies"
+apt-get update
 apt-get --yes install imagemagick tree lftp lighttpd  boxes dialog unattended-upgrades 
 
 echo "Enable automatic installation of security updates"
 sudo dpkg-reconfigure -plow unattended-upgrades
 
 echo "Setup lighttpd"
-sudo groupadd www-data
-sudo usermod -G www-data -a pi
-sudo chown -R www-data:www-data /var/www/html
-sudo chmod -R 775 /var/www/html
-sudo service lighttpd force-reload
+groupadd www-data
+usermod -G www-data -a pi
+chown -R www-data:www-data /var/www/html
+chmod -R 775 /var/www/html
+service lighttpd force-reload
 
 echo "Server Mode Setup: Disable WIFI Power Saving"
-sudo iwconfig wlan0 power off
-sudo echo "wireless-power off"  >> /etc/network/interfaces
+iwconfig wlan0 power off
+echo "wireless-power off"  >> /etc/network/interfaces
 
 echo "Disable unneeded components"
-sudo /opt/vc/bin/tvservice -o
+/opt/vc/bin/tvservice -o
 
 echo "Configure Service" | boxes -d boy -ac
 cp ./src/config/survillancecam.service /etc/systemd/system/
 
 echo "Enable Service at System Start"
 echo 
-sudo systemctl enable survillancecam.service
+systemctl enable survillancecam.service
 echo
 echo "Start Service at System Start"
-sudo systemctl start survillancecam.service
+systemctl start survillancecam.service
 echo
 echo "Check Service is running"
-sudo systemctl status survillancecam.service
+systemctl status survillancecam.service
 echo
 echo "Run ./config.sh to configure the software"
 echo
