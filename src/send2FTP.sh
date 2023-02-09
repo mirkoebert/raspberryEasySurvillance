984838edbacb346eed540e4809dbe0bf2fbe48f2
mirkoebert <mirko@ebert-p.com>
2023-02-09 20:49:04 +0100

    refactor: extract send 2 ftp server

---
+++ src/send2FTP.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+##################################################################
+# This scripts sends a file to the configured FTP server 
+##################################################################
+
+
+set -x
+. ./config
+
+sendToFtpServer(){
+	if [ -n "$FTP_SERVER_RECORDINGS" ]; then
+		curl -q -sS --netrc-file /home/pi/.netrc -T "$1" "ftp://$FTP_SERVER_RECORDINGS"
+	fi
+}
+
