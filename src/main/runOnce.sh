df5767e0167091eddc89aef5d0264fc07dcd6b23
Mirko Ebert <mirko@ebert-p.com>
2026-09-10 20:34:18 +0200

    refactor: improve names

--- src/main/runOnce.sh
+++ src/main/runOnce.sh
@@ -17,31 +17,34 @@
 
 exitIfBlackImage(){
 	thresh=0.02 # (XX as fraction between 0 and 1)
-	mean=$(convert "cam/$DATE" -format "%[mean]" info:)
+	mean=$(convert "cam/$newImageName" -format "%[mean]" info:)
 	meantest=$(convert xc: -format "%[fx:($mean/quantumrange)<$thresh?1:0]" info:)
 	if [ "$meantest" -eq 1 ]; then
-		rm -f "cam/$DATE"
+		rm -f "cam/$newImageName"
 		exit
 	fi
 }
 
-# TODO use global vars for file names
-prevImage=$(ls -r cam/ | head -n 1)
+
 camid=$(hostname)
 
-DATE=$(date +"%Y-%m-%d_%H:%M:%S")_$camid.jpg
-rpicam-still --rotation 270 --width 1296 --height 972 --timeout 1  --nopreview --quality 12  -o "cam/$DATE"
+newImageName=$(newImageName +"%Y-%m-%d_%H:%M:%S")_$camid.jpg
+rpicam-still --rotation 270 --width 1296 --height 972 --timeout 1  --nopreview --quality 12  -o "cam/$newImageName"
 exitIfBlackImage
 
-cp "cam/$DATE" /var/www/html/snapshot.jpg
+# TODO check if this is the right dir
+cp "cam/$newImageName" /var/www/html/snapshot.jpg
 
 
-latestNImage="blur/$DATE.mpc"
-convert -colorspace LinearGray -normalize -blur 2x2  "cam/$DATE" "$latestNImage"
+newImageNormalizedName="blur/$newImageName.mpc"
+convert -colorspace LinearGray -normalize -blur 2x2  "cam/$newImageName" "$newImageNormalizedName"
+
+# TODO use global vars for file names
+prevImage=$(ls -r cam/ | head -n 1)
 
 if [ -n "$prevImage" ]; then
 	file2=$(ls -r blur/*.mpc | head -n 2 | tail -n 1)
-	val=$(compare  -fuzz 10% -metric AE "$file2" "$latestNImage" null: 2>&1)
+	val=$(compare  -fuzz 10% -metric AE "$file2" "$newImageNormalizedName" null: 2>&1)
 	val=$(printf '%.0f' "$val")
 	file2Cache=$(ls -r blur/*.cache | head -n 2 | tail -n 1)
 	rm -f "$file2" "$file2Cache"
@@ -49,9 +52,11 @@ if [ -n "$prevImage" ]; then
 
 	if [ "$val" -gt 1000 ]; then
 		./src/main/reconnectWifi.sh
-		cp "cam/$DATE" "ftp/"
-		sendToFtpServer "ftp/$DATE"
-		rm -f "ftp/$DATE"
+		cp "cam/$newImageName" "ftp/"
+		sendToFtpServer "ftp/$newImageName"
+		rm -f "ftp/$newImageName"
 	fi
 	rm -f "cam/$prevImage"
+else
+  echo "No previous image exists. This is Ok after demon start.";
 fi
